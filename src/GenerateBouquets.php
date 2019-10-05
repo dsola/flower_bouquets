3f76722391acc966cb487b932c6594534727f609
David Sola <d.sola.03@gmail.com>
2019-10-05 15:40:55 +0200

    First commit with main class

---
+++ src/GenerateBouquets.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+final class GenerateBouquets
+{
+    public function __construct()
+    {
+    }
+
+    public function __invoke(string $filePath): void
+    {
+        // TODO: Implement __invoke() method.
+    }
+}
---
+++ tests/FlowersAndBouquetDesignsAreReadFromFileTest.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class FlowersAndBouquetDesignsAreReadFromFileTest extends TestCase
+{
+    public function empty_file_is_processed()
+    {
+
+    }
+}
